6a47914d2bba039b219724d74a3caa635b6a3dca
teosierrar <teosierram@gmail.com>
2025-01-28 16:30:40 +0100

    added for examples

---
+++ for_example.py
@@ -0,0 +1,9 @@
+sum=0
+for x in range(1,101):
+    sum=sum+x
+print(sum)
+
+#lets print the multiplication table until 10
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"{i} X {j} = {i*j}")
